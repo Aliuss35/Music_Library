352682c8e719fcf84a18d2cdbb8aeb1037e39e0c
ali <aliyavuz2000@hotmail.com>
2022-08-05 15:08:13 -0400

    music table in database created

--- musics/models.py
+++ musics/models.py
@@ -1,3 +1,10 @@
+from turtle import mode, title
 from django.db import models
 
 # Create your models here.
+class Music(models.Model):
+  title = models.CharField(max_length=250);
+  artist = models.CharField(max_length=250)
+  album = models.CharField(max_length=250)
+  release_date = models.DateField(auto_now=True)
+  genre = models.CharField(max_length=250)
