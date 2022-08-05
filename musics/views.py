14d9378f1e79994d585b0e6a0c72d7c5cf3534f0
ali <aliyavuz2000@hotmail.com>
2022-08-05 16:49:59 -0400

    GET Method added

--- musics/views.py
+++ musics/views.py
@@ -1,6 +1,13 @@
+from pickle import TRUE
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from .serializers import MusicSerializer
+from .models import Music
+
 
 @api_view(['GET'])
 def musics_list(request):
-  return Response('OK')
+  musics = Music.objects.all()
+  serializer = MusicSerializer(musics, many=TRUE)
+  
+  return Response(serializer.data)
